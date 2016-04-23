1899c91fcb95f8d1a8a2fb4a0ce7b8ae20f67200
emrinalgr <emrinalgr@mycompany.com>
2016-04-23 16:11:34 +0000

    Remove unused pom.xml
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Libraries/uvms-test@11652 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/BaseArquillianTest.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/BaseArquillianTest.java
@@ -1,34 +1,5 @@
 package eu.europa.ec.fisheries.uvms;
 
-import java.io.File;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.jboss.shrinkwrap.resolver.api.maven.ScopeType;
-
-public class BaseArquillianTest {
-
-    @Deployment
-    public static WebArchive createDeployment() {
-        WebArchive webArchive = ShrinkWrap.create(WebArchive.class).addPackages(true, "eu.europa.ec.fisheries.uvms.spatial")
-
-                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
-                .addAsResource("META-INF/orm.xml")
-                .addAsResource("config.properties")
-                .addAsResource("app.properties")
-                .addAsResource("logback.xml")
-                .addAsResource("Config.json")
-                .addAsResource("UserConfig.json")
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-
-        File[] libs = Maven.resolver().loadPomFromFile("pom.xml").importDependencies(ScopeType.COMPILE, ScopeType.RUNTIME, ScopeType.TEST).resolve().withTransitivity().asFile();
-        webArchive = webArchive.addAsLibraries(libs);
-
-        System.out.println(webArchive.toString(true));
-
-        return webArchive;
-    }
+public abstract class BaseArquillianTest {
 
 }
