31dee4d2918eb743962fc126dfa618a8ec4ba399
EMRINALGR <EMRINALGR@mycompany.com>
2016-07-13 08:03:11 +0000

    Try to fix tests
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Libraries/uvms-test@13329 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/BaseDAOTest.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/BaseDAOTest.java
@@ -1,6 +1,5 @@
 package eu.europa.ec.fisheries.uvms;
 
-import com.ninja_squad.dbsetup.DbSetupTracker;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.h2.jdbcx.JdbcConnectionPool;
@@ -16,9 +15,7 @@ public abstract class BaseDAOTest extends BaseUnitilsTest {
 
     private final String TEST_DB_USER = "sa";
     private final String TEST_DB_PASSWORD = "";
-    private final String TEST_DB_URL = "jdbc:h2:mem:testdb;FILE_LOCK=FS;INIT=CREATE SCHEMA IF NOT EXISTS "+ getSchema() +";DATABASE_TO_UPPER=false;TRACE_LEVEL_SYSTEM_OUT=1;MVCC=true";
-
-    protected static DbSetupTracker dbSetupTracker = new DbSetupTracker();
+    private final String TEST_DB_URL = "jdbc:h2:mem:testdb;LOCK_MODE=0;INIT=CREATE SCHEMA IF NOT EXISTS "+ getSchema() +";DATABASE_TO_UPPER=false;TRACE_LEVEL_SYSTEM_OUT=1";
 
     protected DataSource ds = JdbcConnectionPool.create(TEST_DB_URL, TEST_DB_USER, TEST_DB_PASSWORD);
 
